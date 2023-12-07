aaf1deea2e1d7078aa7af1bd9c1fa81cd77aa846
SamratCodess <samratcodess@gmail.com>
2023-12-07 22:28:25 +0545

    home

---
+++ script.js
@@ -0,0 +1,9 @@
+   const typed = new Typed('.multiple-text',
+   {
+    strings : ['Frontend developer','Content Creator','Microsoft learn Student ambassador'],
+    typeSpeed : 50,
+    backSpeed :50,
+    backDelay : 1000,
+    loop : true
+   });
+  
