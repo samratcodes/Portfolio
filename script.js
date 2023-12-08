c440518bd9e84e8cab5a59008bc7de848c91faee
SamratCodess <samratcodess@gmail.com>
2023-12-08 20:15:10 +0545

    Home complete

--- script.js
+++ script.js
@@ -6,4 +6,13 @@
     backDelay : 1000,
     loop : true
    });
-  
+   const header =document.querySelector('header');
+   const bars=document.querySelector('.bars');
+   const xmark=document.querySelector('.cross')
+   const menuicon= document.querySelector('.menu');
+   menuicon.addEventListener('click',function(){
+bars.classList.toggle('activemenu');
+xmark.classList.toggle('activemenu');
+header.style.display=header.style.display=='flex'?'none':'flex';
+
+   })
